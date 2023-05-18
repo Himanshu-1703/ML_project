08e76eac9cf91f129ab2c78370bef818f5268722
Himanshu Arora <himanshu1703@gmail.com>
2023-05-18 20:28:08 +0530

    created setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+
+def get_requirements(file_path):
+    '''
+    This function will return the list of requirements
+    '''
+    hyph_dot = '-e .'
+    requirements = []
+    with open(file=file_path,mode='r') as f:
+        requirements = f.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+        if hyph_dot in requirements:
+            requirements.remove(hyph_dot)
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='himanshu',
+    author_email='himanshu1703@gmail.com',
+    packages=find_packages(),
+    requires=get_requirements('requirements.txt')
+    )
+
